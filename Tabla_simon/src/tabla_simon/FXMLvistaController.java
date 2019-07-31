130893362450f8a91b2c564d28c33b00e7fe1f6b
SimonFV <47420204+SimonFV@users.noreply.github.com>
2019-07-31 16:12:02 -0600

    Add files via upload

--- Tabla_simon/src/tabla_simon/FXMLvistaController.java
+++ Tabla_simon/src/tabla_simon/FXMLvistaController.java
@@ -1,7 +1,17 @@
 package tabla_simon;
 
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,13 +19,13 @@ import javafx.scene.control.Label;
 
 public class FXMLvistaController implements Initializable {
     
-    @FXML
-    private Label label;
+    static String xStrPath;
+    static double[][] myArray;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void handleButtonAction(ActionEvent event){
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        numficom();
     }
     
     //Primera ejecucion.
@@ -24,4 +34,37 @@ public class FXMLvistaController implements Initializable {
         // TODO
     }    
     
+    //Arma el Array con los datos del csv
+    public static void numficom(){
+        Scanner scanIn = null;
+        int filas = 0;
+        int colum = 0;
+        String Inputline = "";
+        double xnum;
+        
+        String xfileLocation;
+        xfileLocation = "C:\\Users\\sfv02\\Downloads\\ISO-Codes.csv";
+        
+        System.out.println("Armando...");
+        
+        try {
+            System.out.print("aqui");
+            scanIn = new Scanner(new BufferedReader(new FileReader("ISO-Codes.csv")));
+            
+            while(scanIn.hasNextLine()){
+                String InputLine = scanIn.nextLine();
+                String[] InArray = InputLine.split(",");
+                
+                for(int x = 0; x < InArray.length; x++){
+                    myArray[filas][x] = Double.parseDouble(InArray[x]);
+                }
+                filas++;
+            }
+            
+            
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
 }
